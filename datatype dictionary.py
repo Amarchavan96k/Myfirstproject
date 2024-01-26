4f9db037fdf47a0cdad1f77ce86fb6cd83bd5790
Amar Chavan <112192738+Amarchavan96k@users.noreply.github.com>
2024-01-26 17:46:56 +0530

    Add files via upload

---
+++ datatype dictionary.py
@@ -0,0 +1,16 @@
+#dictionary in python
+dicteg={"first_name":"amar","mid_name":"vaijinath","last_name":"chavan"}
+print("values in dictionary=",dicteg,"\ntype=",type(dicteg))
+print("==============================================")
+print("firstname=",dicteg["first_name"])
+print("fathername=",dicteg["mid_name"])
+print("sirname=",dicteg["last_name"])
+print("===============================================")
+print("All Keys in the dictionary=",dicteg.keys())
+print("===============================================")
+print("All Values in the dictionary=",dicteg.values())
+print("===============================================")
+dicteg["first_name"]="vaibhav"
+print("after updating the values in dictionary=",dicteg)
+print("===============================================")
+
